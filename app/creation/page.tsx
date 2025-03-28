b08a0e7b2ab2dace4e92c61559fc9032a2f703f4
Aurélie Blanc <aurelie.blanc@oclock.school>
2025-03-28 18:52:47 +0100

    test image bug

--- app/creation/page.tsx
+++ app/creation/page.tsx
@@ -105,6 +105,7 @@ export default function CreationPage () {
                                     alt="logo"
                                     width={300}
                                     height={300}
+                                    unoptimized
                                 />
                         </div>
 
